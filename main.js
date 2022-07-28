7db909051282c2ecd58399c8f4f4b7d114a988c3
Annie Huang <annie.hn@gmail.com>
2022-07-28 20:20:56 +1000

    Change nav-grid.closed to closed-clip-path

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const navGrid = document.querySelector('.nav-grid');
+const byWhite = document.querySelector('.bg-white');
 
 menuToggle.addEventListener('click', () => {
-  navGrid.classList.toggle('closed');
+  navGrid.classList.toggle('closed-clip-path');
+  byWhite.classList.toggle('closed-clip-path');
 });
