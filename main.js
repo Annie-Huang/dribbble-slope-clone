951559a05784785207905283b6872d43e6465d5c
Annie Huang <annie.hn@gmail.com>
2022-07-28 21:01:41 +1000

    .toggleAttribute('data-big-nav-open')

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const menuToggle = document.querySelector('.menu-toggle');
-const navGrid = document.querySelector('.nav-grid');
-const byWhite = document.querySelector('.bg-white');
+const mainGrid = document.querySelector('.main-grid');
 
 menuToggle.addEventListener('click', () => {
-  navGrid.classList.toggle('closed-clip-path');
-  byWhite.classList.toggle('closed-clip-path');
+  mainGrid.toggleAttribute('data-big-nav-open');
 });
