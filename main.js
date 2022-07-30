82083a598097e1e7799ae80c82b165c050d2ede8
Annie Huang <annie.hn@gmail.com>
2022-07-30 12:22:58 +1000

    Add aria-expanded info

--- main.js
+++ main.js
@@ -3,4 +3,12 @@ const mainGrid = document.querySelector('.main-grid');
 
 menuToggle.addEventListener('click', () => {
   mainGrid.toggleAttribute('data-big-nav-open');
+
+  const menuState = menuToggle.getAttribute('aria-expanded');
+  console.log(menuState);
+  if (menuState == 'false') {
+    menuToggle.setAttribute('aria-expanded', true);
+  } else {
+    menuToggle.setAttribute('aria-expanded', false);
+  }
 });
